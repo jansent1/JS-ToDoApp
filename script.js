70a83ebc75f4d3748da0010e9391918945056913
Teun Jansen <teunjansen91@gmail.com>
2024-03-15 07:59:26 +0100

    HTML, CSS, JS varaibles and Btn eventListeners

---
+++ script.js
@@ -0,0 +1,36 @@
+// Variables for html elements:
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");
+const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
+
+// Array to store the tasks along with associated data:
+const taskData = [];
+
+// Object to track state of the tasks when editing and discarding:
+let currentTask = {};
+
+// Button event listeners:
+openTaskFormBtn.addEventListener("click", () => {
+  taskForm.classList.toggle("hidden");
+});
+
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+});
+
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
